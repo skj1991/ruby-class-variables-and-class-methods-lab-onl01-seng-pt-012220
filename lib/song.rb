eaa353f9b88bd763ea373e78e5ed534494ae6fa8
Stacy Johns <stacykjohns@icloud.com>
2020-02-11 02:20:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+end
